79b133b038e6e36a206969d48d6da2ef41b7faf9
Chris Roberts <chrisroberts.code@gmail.com>
2015-03-02 09:25:57 -0800

    Fill in resolve command but only output, no writing or option support

---
+++ lib/batali/command/resolve.rb
@@ -0,0 +1,45 @@
+require 'batali'
+
+module Batali
+  class Command
+
+    # Resolve cookbooks
+    class Resolve < Command
+
+      def execute!
+        file = BFile.new(File.join(Dir.pwd, 'Batali'))
+        system = Grimoire::System.new
+        run_action 'Loading sources' do
+          file.source.map(&:units).flatten.map do |unit|
+            system.add_unit(unit)
+          end
+          nil
+        end
+        requirements = Grimoire::RequirementList.new(
+          :name => :batali_resolv,
+          :requirements => file.cookbook.map{ |ckbk|
+            [ckbk.name, *(ckbk.constraint ? ckbk.constraint : '> 0')]
+          }
+        )
+        solv = Grimoire::Solver.new(
+          :requirements => requirements,
+          :system => system
+        )
+        results = []
+        run_action 'Resolving dependency constraints' do
+          results = solv.generate!
+          nil
+        end
+        if(results.empty?)
+          ui.error 'No solutions found defined requirements!'
+        else
+          ideal_solution = results.pop
+          ui.info "Found #{results.size} solutions. Ideal solution:"
+          ui.puts ideal_solution.units.sort_by(&:name).map{|u| "#{u.name}<#{u.version}>"}
+        end
+      end
+
+    end
+
+  end
+end
