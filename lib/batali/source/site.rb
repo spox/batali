37b2f10e11a14f3e720e34e23dbb4076fcdeb294
Chris Roberts <chrisroberts.code@gmail.com>
2015-03-02 09:29:26 -0800

    Start filling out the site source

---
+++ lib/batali/source/site.rb
@@ -0,0 +1,45 @@
+require 'batali'
+require 'http'
+
+module Batali
+  class Source
+    # Site based source
+    class Site < Source
+
+      # @return [Array<Hash>] dependency strings
+      attr_reader :dependencies
+      # @return [String] version
+      attr_reader :version
+
+      attribute :url, String, :required => true
+
+      # Extract extra info before allowing super to load data
+      #
+      # @param args [Hash]
+      # @return [self]
+      def initialize(args={})
+        @version = args.delete(:version)
+        unless(version)
+          raise ArgumentError.new 'Missing required option `:version`!'
+        end
+        @deps = args.delete(:dependencies) || {}
+        super
+      end
+
+      # @return [VERSION_CLASS]
+      def unit_version
+        version
+      end
+
+      # @return [Array<Grimoire::DEPENDENCY_CLASS>]
+      def unit_dependencies
+        deps.to_a
+      end
+
+      # @return [String] directory
+      def asset
+      end
+
+    end
+  end
+end
