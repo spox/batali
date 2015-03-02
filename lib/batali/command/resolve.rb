40f8b432c4f3c1fa31c2919d39c3c619218904b2
Chris Roberts <chrisroberts.code@gmail.com>
2015-03-02 10:56:16 -0800

    Remove the requiement inspection. It's just ugly and big.

--- lib/batali/command/resolve.rb
+++ lib/batali/command/resolve.rb
@@ -34,7 +34,7 @@ module Batali
           ui.error 'No solutions found defined requirements!'
         else
           ideal_solution = results.pop
-          ui.info "Found #{results.size} solutions for #{requirements.inspect}"
+          ui.info "Found #{results.size} solutions for defined requirements."
           ui.info 'Ideal solution:'
           ui.puts ideal_solution.units.sort_by(&:name).map{|u| "#{u.name}<#{u.version}>"}
         end
