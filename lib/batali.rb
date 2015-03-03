a3bcec36e60b6034eedaff8cf77d478cfff50cdc
Chris Roberts <chrisroberts.code@gmail.com>
2015-03-03 07:50:33 -0800

    Add loaders for manifest and score keeper. Provide customized classes to make things look nicer.

--- lib/batali.rb
+++ lib/batali.rb
@@ -3,11 +3,16 @@ require 'grimoire'
 
 module Batali
 
-  VERSION_CLASS = Grimoire::VERSION_CLASS
+  # Simple stubs mostly for naming
+  class UnitVersion < Grimoire::VERSION_CLASS; end
+  class UnitDependency < Grimoire::DEPENDENCY_CLASS; end
+  class UnitRequirement < Grimoire::REQUIREMENT_CLASS; end
 
   autoload :Command, 'batali/command'
   autoload :Config, 'batali/config'
+  autoload :Manifest, 'batali/manifest'
   autoload :RemoteSite, 'batali/remote_site'
+  autoload :ScoreKeeper, 'batali/score_keeper'
   autoload :Source, 'batali/source'
   autoload :Unit, 'batali/unit'
 
@@ -15,3 +20,11 @@ end
 
 require 'batali/b_file'
 require 'batali/version'
+
+Grimoire.send(:remove_const, :VERSION_CLASS)
+Grimoire.send(:remove_const, :DEPENDENCY_CLASS)
+Grimoire.send(:remove_const, :REQUIREMENT_CLASS)
+
+Grimoire.const_set(:VERSION_CLASS, Batali::UnitVersion)
+Grimoire.const_set(:DEPENDENCY_CLASS, Batali::UnitDependency)
+Grimoire.const_set(:REQUIREMENT_CLASS, Batali::UnitRequirement)
