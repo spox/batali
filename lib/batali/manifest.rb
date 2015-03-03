0fb11ebb810904031990fe2ab75cf2304f0f123b
Chris Roberts <chrisroberts.code@gmail.com>
2015-03-03 09:54:03 -0800

    Add helper for building manifest instance. Allow manifest to be empty (when no serialized file exists)

--- lib/batali/manifest.rb
+++ lib/batali/manifest.rb
@@ -4,14 +4,31 @@ module Batali
   # Collection of resolved units
   class Manifest < Grimoire::Utility
 
-    attribute :cookbook, Unit, :multiple => true, :required => true
+    attribute :cookbook, Unit, :multiple => true
+
+    # Build manifest from given path. If no file exists, empty
+    # manifest will be provided.
+    #
+    # @param path [String] path to manifest
+    # @return [Manifest]
+    def self.build(path)
+      if(File.exists?(path))
+        self.new(
+          :cookbook => Bogo::Config.new(
+            :path => path
+          ).data[:cookbook]
+        )
+      else
+        self.new
+      end
+    end
 
     # Check for unit within manifest
     #
     # @param unit [Unit]
     # @return [TrueClass, FalseClass]
     def include?(unit)
-      cookbook.include?(unit)
+      cookbook && cookbook.include?(unit)
     end
 
   end
