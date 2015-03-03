b711a70e7338bffe774c8eeee9b4840334e5fb53
Chris Roberts <chrisroberts.code@gmail.com>
2015-03-03 14:48:54 -0800

    Provide expected dry-run behavior. Fix requirement setup when no constraint is provided.

--- lib/batali/command/resolve.rb
+++ lib/batali/command/resolve.rb
@@ -19,7 +19,7 @@ module Batali
         requirements = Grimoire::RequirementList.new(
           :name => :batali_resolv,
           :requirements => batali_file.cookbook.map{ |ckbk|
-            [ckbk.name, *(ckbk.constraint ? ckbk.constraint : '> 0')]
+            [ckbk.name, *(ckbk.constraint.empty? ? ['> 0'] : ckbk.constraint)]
           }
         )
         solv = Grimoire::Solver.new(
@@ -36,13 +36,18 @@ module Batali
           ui.error 'No solutions found defined requirements!'
         else
           ideal_solution = results.pop
+          dry_run('manifest file write') do
+            run_action 'Writing manifest' do
+              manifest = Manifest.new(:cookbook => ideal_solution.units)
+              File.open('batali.manifest', 'w') do |file|
+                file.write MultiJson.dump(manifest, :pretty => true)
+              end
+              nil
+            end
+          end
           ui.info "Found #{results.size} solutions for defined requirements."
           ui.info 'Ideal solution:'
           ui.puts ideal_solution.units.sort_by(&:name).map{|u| "#{u.name}<#{u.version}>"}
-          manifest = Manifest.new(:cookbook => ideal_solution.units)
-          File.open('batali.manifest', 'w') do |file|
-            file.write MultiJson.dump(manifest, :pretty => true)
-          end
         end
       end
 
