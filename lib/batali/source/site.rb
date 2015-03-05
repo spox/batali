91eaa32e73b0ac8b8d3955cf4d8ff83a0de25b37
Chris Roberts <chrisroberts.code@gmail.com>
2015-03-04 21:02:38 -0800

    Implement asset for site source

--- lib/batali/source/site.rb
+++ lib/batali/source/site.rb
@@ -1,5 +1,8 @@
 require 'batali'
 require 'http'
+require 'tmpdir'
+require 'rubygems/package'
+require 'zlib'
 
 module Batali
   class Source
@@ -35,6 +38,31 @@ module Batali
 
       # @return [String] directory
       def asset
+        path = Dir.mktmpdir('batali')
+        result = HTTP.get(url)
+        while(result.code == 302)
+          result = HTTP.get(result.headers['Location'])
+        end
+        File.open(a_path = File.join(path, 'asset'), 'w') do |file|
+          while(content = result.body.readpartial(2048))
+            file.write content
+          end
+        end
+        ext = Gem::Package::TarReader.new(
+          Zlib::GzipReader.open(a_path)
+        )
+        ext.rewind
+        ext.each do |entry|
+          next unless entry.file?
+          n_path = File.join(path, entry.full_name)
+          FileUtils.mkdir_p(File.dirname(n_path))
+          File.open(n_path, 'w') do |file|
+            while(content = entry.read(2048))
+              file.write(content)
+            end
+          end
+        end
+        path
       end
 
     end
