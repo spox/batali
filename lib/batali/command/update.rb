a8ebc73f6a6930afabf735a33a121eff4c3d1966
Chris Roberts <chrisroberts.code@gmail.com>
2015-03-05 20:24:07 -0800

    Let update combine resolve and install

--- lib/batali/command/update.rb
+++ lib/batali/command/update.rb
@@ -6,6 +6,11 @@ module Batali
     # Update cookbook manifest
     class Update < Batali::Command
 
+      def execute!
+        Resolve.new({:resolve => opts}, arguments).execute!
+        Install.new({:install => opts}, arguments).execute!
+      end
+
     end
 
   end
