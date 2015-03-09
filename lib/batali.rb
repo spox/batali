d17d5e6eba9b6a13af4b8cb26529419a5919cdd9
Chris Roberts <chrisroberts.code@gmail.com>
2015-03-09 13:30:00 -0700

    Load the git helper module

--- lib/batali.rb
+++ lib/batali.rb
@@ -5,6 +5,7 @@ module Batali
 
   autoload :Command, 'batali/command'
   autoload :Config, 'batali/config'
+  autoload :Git, 'batali/git'
   autoload :Manifest, 'batali/manifest'
   autoload :Origin, 'batali/origin'
   autoload :ScoreKeeper, 'batali/score_keeper'
