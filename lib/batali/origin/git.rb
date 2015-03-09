e81c28b3cffbe6188f8c6f5da0a629a4aa53ed9e
Chris Roberts <chrisroberts.code@gmail.com>
2015-03-09 13:32:32 -0700

    Extract git action methods and use common module

--- lib/batali/origin/git.rb
+++ lib/batali/origin/git.rb
@@ -6,9 +6,7 @@ module Batali
     # Fetch unit from local path
     class Git < Path
 
-      attribute :url, String, :required => true
-      attribute :ref, String, :required => true
-      attribute :cache, String, :required => true
+      include Batali::Git
 
       def initialize(args={})
         unless(args[:path])
@@ -49,43 +47,6 @@ module Batali
           clone_repository
           ref_dup
         end
-      end
-
-      # @return [String] path to repository clone
-      def base_path
-        File.join(cache, Base64.urlsafe_encode64(url))
-      end
-
-      # Clone the repository to the local machine
-      #
-      # @return [TrueClass]
-      def clone_repository
-        if(File.directory?(base_path))
-          repo = ::Git.open(base_path)
-          repo.checkout('master')
-          repo.pull
-          repo.fetch
-        else
-          ::Git.clone(url, base_path)
-        end
-        true
-      end
-
-      # Duplicate reference and store
-      #
-      # @return [String] commit SHA
-      # @note this will update ref to SHA
-      def ref_dup
-        git = ::Git.open(base_path)
-        git.checkout(ref)
-        self.ref = git.log.first.sha
-        self.path = File.join(cache, ref)
-        unless(File.directory?(path))
-          FileUtils.mkdir_p(path)
-          FileUtils.cp_r(File.join(base_path, '.'), path)
-          FileUtils.rm_rf(File.join(path, '.git'))
-        end
-        self.path
       end
 
     end
