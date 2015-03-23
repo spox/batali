e9bdd71bd458d82dc056aa0489807af30701e97e
Chris Roberts <chrisroberts.code@gmail.com>
2015-03-23 08:05:07 -0700

    Update scoring implementation to provide preference within segement, within parent segment, and finally default score. Specify highscore preference.

--- lib/batali/score_keeper.rb
+++ lib/batali/score_keeper.rb
@@ -6,12 +6,43 @@ module Batali
 
     attribute :manifest, Manifest, :required => true
 
+    # Always prefer higher scoring units
+    #
+    # @return [Symbol] :highscore
+    def preferred_score
+      :highscore
+    end
+
     # Provide score for given unit
     #
-    # @param unit [Unit]
+    # @param unit [Unit] unit to score
+    # @param idx [Integer] current index location
     # @return [Numeric, NilClass]
-    def score_for(unit)
-      manifest.include?(unit) ? 0 : nil
+    def score_for(unit, idx)
+      multiplier = 1
+      manifest_unit = manifest.cookbook.detect do |m_unit|
+        m_unit.name == unit.name
+      end
+      if(manifest_unit)
+        if(manifest_unit.version == unit.version)
+          multiplier = 100000
+        else
+          if(UnitRequirement.new("~> #{manifest_unit.version}").satisfied_by?(unit.version))
+            multiplier = 10000
+          elsif(UnitRequirement.new("~> #{manifest_unit.version.segments.slice(0,2).join('.')}").satisfied_by?(unit.version))
+            multiplier = 1000
+          end
+        end
+      end
+      score = []
+      unit.version.segments.reverse.each_with_index.map do |val, pos|
+        if(val == 0)
+          score.push 0
+        else
+          score.push (2 - (1.0 / val)) * ((pos + 1)**10)
+        end
+      end
+      score.inject(&:+) * multiplier
     end
 
   end
