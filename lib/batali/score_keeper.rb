e5ede1d567127f1dc4e567dc6e03ae92cb1cb273
Chris Roberts <chrisroberts.code@gmail.com>
2015-03-23 10:57:55 -0700

    Add some comments to how we are scoring our versions within the score keeper

--- lib/batali/score_keeper.rb
+++ lib/batali/score_keeper.rb
@@ -24,12 +24,20 @@ module Batali
         m_unit.name == unit.name
       end
       if(manifest_unit)
+        # If the unit version matches the manifest version, this
+        # should be _the_ preferred version
         if(manifest_unit.version == unit.version)
           multiplier = 10000000
         else
+          # If the unit version satisfies within the patch segment of
+          # the manifest version score those versions highest for upgrade
           if(UnitRequirement.new("~> #{manifest_unit.version}").satisfied_by?(unit.version))
             multiplier = 1000000
           else
+            # If the unit version satisfies within the minor or major
+            # version segments of the manifest version, bump score
+            # value up (with satisfaction within minor segment being
+            # worth more than satisfaction within major segment)
             pos = UnitRequirement.new("~> #{manifest_unit.version.segments.slice(0,2).join('.')}").satisfied_by?(unit.version) ? 1 : 0
             multi_val = pos == 1 ? 1000 : 100
             distance = (manifest_unit.version.segments[pos] - unit.version.segments[pos])
@@ -43,6 +51,8 @@ module Batali
         end
       end
       score = []
+      # Generate a "value" for each segment of the version with
+      # growing importance (major > minor > patch)
       unit.version.segments.reverse.each_with_index.map do |val, pos|
         if(val == 0)
           score.push 0
@@ -50,6 +60,10 @@ module Batali
           score.push (2 - (1.0 / val)) * ((pos + 1)**10)
         end
       end
+      # Sum the score for each segment to provide the score for the
+      # version and multiply by defined multiplier to force higher
+      # score when manifest drives desire for versions closer to
+      # version defined within manifest
       score = score.inject(&:+) * multiplier
       debug "Score <#{unit.name}:#{unit.version}>: #{score}"
       score
