90609cb061db68308dcc7ab6e9b35df1929c158f
Chris Roberts <chrisroberts.code@gmail.com>
2015-03-23 08:42:38 -0700

    Prefer latest available versions of segments _closer_ to version set within manifest

--- lib/batali/score_keeper.rb
+++ lib/batali/score_keeper.rb
@@ -25,12 +25,26 @@ module Batali
       end
       if(manifest_unit)
         if(manifest_unit.version == unit.version)
-          multiplier = 100000
+          multiplier = 10000000
         else
           if(UnitRequirement.new("~> #{manifest_unit.version}").satisfied_by?(unit.version))
-            multiplier = 10000
+            multiplier = 1000000
           elsif(UnitRequirement.new("~> #{manifest_unit.version.segments.slice(0,2).join('.')}").satisfied_by?(unit.version))
-            multiplier = 1000
+            distance = (manifest_unit.version.segments[1] - unit.version.segments[1])
+            if(distance > 0)
+              distance = 1.0 / distance
+            else
+              distance = 0
+            end
+            multiplier = 1000 + (1000 * distance)
+          else
+            distance = (manifest_unit.version.segments.first - unit.version.segments.first)
+            if(distance > 0)
+              distance = 1.0 / distance
+            else
+              distance = 0
+            end
+            multiplier = 100 + (100 * distance)
           end
         end
       end
@@ -42,7 +56,9 @@ module Batali
           score.push (2 - (1.0 / val)) * ((pos + 1)**10)
         end
       end
-      score.inject(&:+) * multiplier
+      score = score.inject(&:+) * multiplier
+      debug "Score <#{unit.name}:#{unit.version}>: #{score}"
+      score
     end
 
   end
