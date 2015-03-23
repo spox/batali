c85f0f0b58c271aaf632a12bf03b81b61c0eadb3
Christopher Kuttruff <kuttruff@gmail.com>
2015-03-23 13:09:28 -0700

    Split up logic for scoring

--- lib/batali/score_keeper.rb
+++ lib/batali/score_keeper.rb
@@ -6,6 +6,10 @@ module Batali
 
     attribute :manifest, Manifest, :required => true
 
+    SCORE_WEIGHT_MAX = 10_000_000
+    SCORE_WEIGHT_MID =  1_000_000
+    SCORE_WEIGHT_MIN =          1
+
     # Always prefer higher scoring units
     #
     # @return [Symbol] :highscore
@@ -23,49 +27,57 @@ module Batali
       manifest_unit = manifest.cookbook.detect do |m_unit|
         m_unit.name == unit.name
       end
-      if(manifest_unit)
-        # If the unit version matches the manifest version, this
-        # should be _the_ preferred version
-        if(manifest_unit.version == unit.version)
-          multiplier = 10000000
-        else
-          # If the unit version satisfies within the patch segment of
-          # the manifest version score those versions highest for upgrade
-          if(UnitRequirement.new("~> #{manifest_unit.version}").satisfied_by?(unit.version))
-            multiplier = 1000000
-          else
-            # If the unit version satisfies within the minor or major
-            # version segments of the manifest version, bump score
-            # value up (with satisfaction within minor segment being
-            # worth more than satisfaction within major segment)
-            pos = UnitRequirement.new("~> #{manifest_unit.version.segments.slice(0,2).join('.')}").satisfied_by?(unit.version) ? 1 : 0
-            multi_val = pos == 1 ? 1000 : 100
-            distance = (manifest_unit.version.segments[pos] - unit.version.segments[pos])
-            if(distance > 0)
-              distance = 1.0 / distance
-            else
-              distance = 0
-            end
-            multiplier = multi_val + (multi_val * distance)
-          end
-        end
-      end
-      score = []
-      # Generate a "value" for each segment of the version with
-      # growing importance (major > minor > patch)
-      unit.version.segments.reverse.each_with_index.map do |val, pos|
-        if(val == 0)
-          score.push 0
-        else
-          score.push (2 - (1.0 / val)) * ((pos + 1)**10)
-        end
-      end
+
+      multiplier = score_multiplier(unit, manifest_unit)
+      score_arr = version_segment_score(unit, multiplier)
       # Sum the score for each segment to provide the score for the
       # version and multiply by defined multiplier to force higher
       # score when manifest drives desire for versions closer to
       # version defined within manifest
-      score = score.inject(&:+) * multiplier
+      score = score_arr.inject(&:+) * multiplier
       debug "Score <#{unit.name}:#{unit.version}>: #{score}"
+      score
+    end
+
+    private
+
+    # Create a score based on the version segments (major, minor, patch)
+    #   Manifest will create a higher score for versions closest to specification
+    #
+    # @param unit [Unit] unit to score
+    # @param multiplier [Numeric] weight based on version constraint
+    # @return [Numeric]
+    def score_multiplier(unit, manifest_unit)
+      return SCORE_WEIGHT_MIN if manifest_unit.nil?
+      # If the unit version matches the manifest version, this
+      # should be _the_ preferred version
+      return SCORE_WEIGHT_MAX if manifest_unit.version == unit.version
+      return SCORE_WEIGHT_MID if UnitRequirement.new("~> #{manifest_unit.version}").satisfied_by?(unit.version)
+
+      unit_req = UnitRequirement.new("~> #{manifest_unit.version.segments.slice(0,2).join('.')}")
+      pos = unit_req.satisfied_by?(unit.version) ? 1 : 0
+      requirement_multiplier = pos == 1 ? 1000 : 100
+      version_distance = manifest_unit.version.segments[pos] - unit.version.segments[pos]
+      version_dist_multiplier = (version_distance > 0) ? (1.0 / distance) : 0
+
+      requirement_multiplier + (requirement_multiplier * version_dist_multiplier)
+    end
+
+    # Create a score for each version segment (major, minor, patch)
+    #   Manifest will create a higher score for versions closest to specification
+    #
+    # @param unit [Unit] unit to score
+    # @param multiplier [Numeric] weight based on version constraint
+    # @return [Numeric]
+    def version_segment_score(unit, multiplier)
+      score = []
+      # Generate a "value" for each segment of the version with
+      # growing importance (major > minor > patch)
+      # TODO: add comment about this math
+      unit.version.segments.reverse.each_with_index.map do |val, pos|
+        res = (val == 0) ? 0 : (2 - (1.0 / val)) * ((pos + 1)**10)
+        score.push(res)
+      end
       score
     end
 
