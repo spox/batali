94e5e4466a3640c768810b3caf1efb80d1c48be2
Chris Roberts <chrisroberts.code@gmail.com>
2015-04-15 06:55:39 -0700

    Provide support for chefspec

---
+++ lib/batali/chefspec.rb
@@ -0,0 +1,53 @@
+require 'batali'
+require 'stringio'
+require 'fileutils'
+require 'tmpdir'
+
+module ChefSpec
+  class Batali
+
+    class << self
+      extend Forwardable
+      def_delegators :instance, :setup!, :teardown!
+    end
+
+    include Singleton
+
+    def initialize
+      @vendor_path = Dir.mktmpdir
+    end
+
+    def setup!
+      output = ''
+      begin
+        ::Batali::Command::Update.new(
+          :file => File.join(Dir.pwd, 'Batali'),
+          :path => @vendor_path,
+          :ui => Bogo::Ui.new(
+            :app_name => 'Batali',
+            :output_to => StringIO.new(output)
+          )
+        ).execute!
+        RSpec.configure do |config|
+          config.cookbook_path = @vendor_path
+        end
+      rescue => e
+        $stderr.puts "Batali failure - #{e.class}: #{e.message}"
+        $stderr.puts output
+        raise
+      end
+    end
+
+    def teardown!
+      if(File.directory?(@vendor_path))
+        FileUtils.rm_rf(@vendor_path)
+      end
+    end
+
+  end
+end
+
+RSpec.configure do |config|
+  config.before(:suite){ ChefSpec::Batali.setup! }
+  config.after(:suite){ ChefSpec::Batali.teardown! }
+end
