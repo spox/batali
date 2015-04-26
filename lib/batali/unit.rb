683a2e86c08bf5cdf67b4947c939267425e37aca
Chris Roberts <chrisroberts.code@gmail.com>
2015-04-26 15:33:38 -0700

    Allow source to be optionally defined for unit

--- lib/batali/unit.rb
+++ lib/batali/unit.rb
@@ -3,7 +3,7 @@ require 'batali'
 module Batali
   # Customized Unit
   class Unit < Grimoire::Unit
-    attribute :source, Source, :required => true, :coerce => lambda{|v| Batali::Source.build(v)}
+    attribute :source, Source, :coerce => lambda{|v| Batali::Source.build(v)}
     attribute :dependencies, [Batali::UnitDependency, Grimoire::DEPENDENCY_CLASS], :multiple => true, :default => [], :coerce => lambda{|val| Batali::UnitDependency.new(val.first, *val.last)}
     attribute :version, [Batali::UnitVersion, Grimoire::VERSION_CLASS], :required => true, :coerce => lambda{|val| Batali::UnitVersion.new(val)}
   end
