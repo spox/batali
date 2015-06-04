e6672ae72118cab8170ff0c08c5b7e945deafcb9
Chris Roberts <code@chrisroberts.org>
2015-06-04 15:21:22 -0700

    Remove files from target when an ignore file is provided (#29)

--- lib/batali/source/path.rb
+++ lib/batali/source/path.rb
@@ -8,6 +8,9 @@ module Batali
     # Path based source
     class Path < Source
 
+      # @return [Array<String>] default ignore globs
+      DEFAULT_IGNORE = ['.git*']
+
       include Bogo::Memoization
 
       attribute :path, String, :required => true, :equivalent => true
@@ -16,7 +19,21 @@ module Batali
       def asset
         memoize(:asset) do
           dir = Dir.mktmpdir
-          FileUtils.cp_r(File.join(path, '.'), dir)
+          chefignore = File.join(path, '.chefignore')
+          chefignore = File.exists?(chefignore) ? File.readlines(chefignore) : []
+          chefignore += DEFAULT_IGNORE
+          chefignore.uniq!
+          files_to_copy = Dir.glob(File.join(path, '{.[^.]*,**}', '**', '{*,*.*,.*}'))
+          files_to_copy = files_to_copy.map do |file_path|
+            next unless File.file?(file_path)
+            relative_path = file_path.sub("#{path}/", '')
+            relative_path unless chefignore.detect{|ig| File.fnmatch(ig, relative_path)}
+          end.compact
+          files_to_copy.each do |relative_path|
+            new_path = File.join(dir, relative_path)
+            FileUtils.mkdir_p(File.dirname(new_path))
+            FileUtils.cp(File.join(path, relative_path), new_path)
+          end
           dir
         end
       end
