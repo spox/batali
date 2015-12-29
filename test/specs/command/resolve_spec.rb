06adcbe991da9dc61315364f70ff2c01b2991993
Chris Roberts <code@chrisroberts.org>
2015-12-29 10:13:53 -0800

    Start inclusion of comprehensive behavior tests

---
+++ test/specs/command/resolve_spec.rb
@@ -0,0 +1,156 @@
+require 'batali'
+require 'tmpdir'
+require 'stringio'
+require 'minitest/autorun'
+
+def quiet_in_directory(dir)
+  o_out = $stdout
+  output = StringIO.new('')
+  $stdout = output
+  Dir.chdir(dir) do
+    yield
+  end
+  $stdout = o_out
+  output.rewind
+  output
+end
+
+describe Batali::Command::Resolve do
+
+  before do
+    @cache = Dir.mktmpdir('batali-cache')
+    FileUtils.mkdir_p(
+      File.join(
+        @cache, 'remote_site',
+        '970d5cda31bf188b52605436a209bb4062e9afef693530b05c029831cf0084e6'
+      )
+    )
+    FileUtils.cp(
+      File.join(File.dirname(__FILE__), 'data', 'universe.json'),
+      File.join(
+        @cache, 'remote_site',
+        '970d5cda31bf188b52605436a209bb4062e9afef693530b05c029831cf0084e6',
+        'universe.json'
+      )
+    )
+    @cwd = Dir.mktmpdir('batali-cwd')
+  end
+
+  after do
+    FileUtils.rm_rf(@cache)
+    FileUtils.rm_rf(@cwd)
+  end
+
+  describe 'Single path resolution' do
+
+    describe 'Single cookbook' do
+
+      before do
+        File.open(File.join(@cwd, 'Batali'), 'w') do |file|
+          file.puts <<-EOF
+Batali.define do
+  source 'https://supermarket.chef.io'
+  cookbook 'users'
+end
+EOF
+        end
+        quiet_in_directory(@cwd) do
+          Batali::Command::Resolve.new(
+            Smash.new(:cache_directory => @cache),
+            Array.new
+          ).execute!
+        end
+      end
+
+      it 'should create manifest file with single entry' do
+        File.exist?(File.join(@cwd, 'batali.manifest')).must_equal true
+        contents = MultiJson.load(File.read(File.join(@cwd, 'batali.manifest'))).to_smash
+        contents['cookbook'].size.must_equal 1
+        contents['cookbook'].first['version'].must_equal '2.0.0'
+      end
+
+    end
+
+    describe 'Multiple cookbooks' do
+      before do
+        File.open(File.join(@cwd, 'Batali'), 'w') do |file|
+          file.puts <<-EOF
+Batali.define do
+  source 'https://supermarket.chef.io'
+  cookbook 'users'
+  cookbook 'chef-server-populator'
+end
+EOF
+        end
+        quiet_in_directory(@cwd) do
+          Batali::Command::Resolve.new(
+            Smash.new(:cache_directory => @cache),
+            Array.new
+          ).execute!
+        end
+      end
+
+      it 'should create manifest file with all entries and dependencies' do
+        File.exist?(File.join(@cwd, 'batali.manifest')).must_equal true
+        contents = MultiJson.load(File.read(File.join(@cwd, 'batali.manifest'))).to_smash
+        expected = Smash.new(
+          'users' => '2.0.0',
+          'chef-server-populator' => '1.2.2',
+          'chef-server' => '4.1.0',
+          'chef-ingredient' => '0.16.0',
+          'apt-chef' => '0.2.2',
+          'apt' => '2.9.2',
+          'yum-chef' => '0.2.2',
+          'yum' => '3.8.2'
+        )
+        contents['cookbook'].size.must_equal expected.size
+        expected.each do |name, version|
+          contents['cookbook'].any? do |cook|
+            cook['name'] == name &&
+              cook['version'] == version
+          end.must_equal true
+        end
+      end
+
+    end
+
+  end
+
+  describe 'Multiple path resolution (infrastructure-mode)' do
+
+    describe 'Single cookbook' do
+
+      before do
+        File.open(File.join(@cwd, 'Batali'), 'w') do |file|
+          file.puts <<-EOF
+Batali.define do
+  source 'https://supermarket.chef.io'
+  cookbook 'users', '> 1.0'
+end
+EOF
+        end
+        quiet_in_directory(@cwd) do
+          Batali::Command::Resolve.new(
+            Smash.new(
+              :cache_directory => @cache,
+              :infrastructure => true
+            ),
+            Array.new
+          ).execute!
+        end
+      end
+
+      it 'should create manifest file with multiple entries' do
+        File.exist?(File.join(@cwd, 'batali.manifest')).must_equal true
+        contents = MultiJson.load(File.read(File.join(@cwd, 'batali.manifest'))).to_smash
+        contents['cookbook'].size.must_equal 14
+        contents['cookbook'].all? do |item|
+          Gem::Version.new(item['version']) > Gem::Version.new('1.0.0')
+        end.must_equal true
+      end
+
+    end
+
+  end
+
+end
