6893f2a272bb6e44e109c46d3adfc0b0021360b0
Chris Roberts <code@chrisroberts.org>
2016-03-29 11:48:30 -0700

    Print raw contents of full solution if debug enabled

--- lib/batali/command/resolve.rb
+++ lib/batali/command/resolve.rb
@@ -74,6 +74,10 @@ module Batali
           ui.error 'No solutions found defined requirements!'
         else
           ideal_solution = results.pop
+          debug 'Full solution raw contents:'
+          ideal_solution.units.each do |unit|
+            debug [unit.name, unit.version].join(' -> ')
+          end
           dry_run('manifest file write') do
             run_action 'Writing manifest' do
               manifest = Manifest.new(
